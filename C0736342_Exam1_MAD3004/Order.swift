d1e45bf05f1e1c55e641c69d00e87d1cc3276aca
MacStudent <macstudent@20X-iMacXX-M-454.local>
2018-07-23 16:52:30 -0400

    first commit

---
+++ C0736342_Exam1_MAD3004/Order.swift
@@ -0,0 +1,9 @@
+//
+//  Order.swift
+//  C0736342_Exam1_MAD3004
+//
+//  Created by MacStudent on 2018-07-23.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import Foundation
---
+++ C0736342_Exam1_MAD3004/Utils.swift
@@ -0,0 +1,9 @@
+//
+//  Utils.swift
+//  C0736342_Exam1_MAD3004
+//
+//  Created by MacStudent on 2018-07-23.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import Foundation
